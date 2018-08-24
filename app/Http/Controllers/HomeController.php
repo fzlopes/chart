63ae4b457225086bf94a0f8bcfa44b0a7eacb7c8
Fábio Lopes <fzlopes1@gmail.com>
2018-08-24 15:03:09 -0300

    Teste com gráficos estáticos.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,12 +26,29 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $chart = Charts::create('line', 'highcharts')
-                    ->title('Comparação Mensal (Últimos 4 anos)')
-                    ->labels(['jan','fev','mar','abr','mai','jun','jul','ago','set','out','nov','dez'])
-                    ->values([5,10,20,13,16,25,12,17,7,9,6,10])
-                    ->dimensions(1000,500)
-                    ->responsive(true);
+//        $chart = Charts::create('line', 'highcharts')
+//                    ->title('Comparação Mensal (Últimos 4 anos)')
+//                    ->labels(['jan','fev','mar','abr','mai','jun','jul','ago','set','out','nov','dez'])
+//                    ->values([5,10,20,13,16,25,12,17,7,9,6,10])
+//                    ->dimensions(1000,500)
+//                    ->responsive(true);
+
+//        $chart = Charts::create('line', 'material')
+//                    ->title('Comparação Mensal (Últimos 4 anos)')
+//                    ->labels(['jan','fev','mar','abr','mai','jun','jul','ago','set','out','nov','dez'])
+//                    ->values([5,10,20,13,16,25,12,17,7,9,6,10])
+//                    ->elementLabel("Total")
+//                    ->dimensions(1000,500)
+//                    ->responsive(true);
+
+        $chart = Charts::create('line', 'google')
+            ->title('Comparação Mensal (Últimos 4 anos)')
+            ->labels(['jan','fev','mar','abr','mai','jun','jul','ago','set','out','nov','dez'])
+            ->values([5,10,20,13,16,25,12,17,7,9,6,10])
+            ->elementLabel("Total")
+            ->dimensions(1000,500)
+            ->responsive(true);
+
         return view('home',compact('chart'));
     }
 }
